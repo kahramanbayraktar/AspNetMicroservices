bc336c5543898f315b7f5b0541136e4db09e8580
Kahraman Bayraktar <kah_ra_man@hotmail.com>
2023-04-29 16:43:37 +0300

    Implement EF persistence layer (Ordering)

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -6,6 +6,7 @@ namespace Ordering.Application.Contracts.Persistence
     public interface IAsyncRepository<T> where T : EntityBase
     {
         Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         string includeString = null,
