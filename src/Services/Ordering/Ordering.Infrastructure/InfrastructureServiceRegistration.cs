3a89f4b3e2a5fc7fe07b7414b134c35f426e20e9
Kahraman Bayraktar <kah_ra_man@hotmail.com>
2023-05-03 08:06:49 +0300

    Fix a service registration

--- src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
 using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
 using Ordering.Infrastructure.Persistence;
 using Ordering.Infrastructure.Repositories;
 
@@ -21,7 +22,7 @@ namespace Ordering.Infrastructure
             services.AddScoped<IOrderRepository, OrderRepository>();
 
             services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
-            services.AddTransient<IEmailService, IEmailService>(); // TODO: why Transient?
+            services.AddTransient<IEmailService, EmailService>(); // TODO: why Transient?
 
             return services;
         }
